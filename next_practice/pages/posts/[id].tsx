837d17502d62571908c35f439c7553d11a314564
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-03-24 02:07:01 +0900

    create static posts page

---
+++ next_practice/pages/posts/[id].tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from 'next';
+
+type PostItem = {
+    id: string
+    body: string
+    title: string
+}
+type PagePropsType = {
+    post: PostItem
+}
+
+const Posts: NextPage<PagePropsType> = ( { post }: PagePropsType ) => {
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <div>
+                <p>{post.body}</p>
+            </div>
+        </>
+    )
+}
+
+type GetStaticPropsType = {
+    params: PostItem
+}
+
+export async function getStaticProps( { params }: GetStaticPropsType ) {
+    const res = await fetch( `https://jsonplaceholder.typicode.com/posts/${params.id}` );
+    const post: PostItem = await res.json();
+
+    return {
+        props: {
+            post
+        }
+    }
+}
+
+type PathsItem = {
+    params: {
+        id: string
+    }
+}
+type PathsType = PathsItem[];
+
+export async function getStaticPaths() {
+    const res = await fetch( 'https://jsonplaceholder.typicode.com/posts' );
+    const posts = await res.json();
+
+    const paths: PathsType = posts.map( ( post: PostItem ) => ( {
+        params: { id: post.id.toString() }
+    } ) )
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default Posts;
